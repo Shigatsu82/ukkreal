257d4e93bd6bc6f087c58092183bc3857b797afc
zulham <zulhamnoor2@gmail.com>
2024-06-14 14:40:20 +0700

    [invent-01] Lihat Semua Kategori

--- app/Http/Controllers/api/KategoriController.php
+++ app/Http/Controllers/api/KategoriController.php
@@ -1,11 +1,26 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $kategori = Kategori::all();
+        $data = array("data"=>$kategori);
+
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    
 }
